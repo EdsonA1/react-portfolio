20274602b0457c0153335fdc86d788e13d8e8118
eaugust2 <123189149+eaugust2@users.noreply.github.com>
2023-10-26 13:30:52 -0400

    added main content and css

--- src/components/Content/MainContent.js
+++ src/components/Content/MainContent.js
@@ -1,10 +1,11 @@
 // src/components/MainContent.js
 import React from 'react';
+import './MainContent.css';
 
 const MainContent = () => {
     return (
         <div className="main-content">
-            <h2>Hi, i'm Edson.A</h2>
+            <h2>Hi, i'm <strong>Edson.A</strong></h2>
             <p>Computer Science, Real Estate, Engineering Corporate Practice</p>
             <p>@University of Notre Dame</p>
         </div>
